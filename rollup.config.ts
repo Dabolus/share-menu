6f7bedb78bb025232a2ff27784d17ac8220862f0
Giorgio Garasto <giorgio@garasto.it>
2018-12-21 21:27:45 +0100

    chore: Build both min and pretty versions of the library in prod mode

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,6 +1,8 @@
 // tslint:disable:object-literal-sort-keys
 import { readdirSync } from 'fs';
 import { resolve } from 'path';
+import minifyHtml from 'rollup-plugin-minify-html-literals';
+import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
 const prod = process.env.NODE_ENV === 'production';
@@ -9,14 +11,14 @@ const inputs = readdirSync('src')
   .filter((file) => !file.endsWith('.d.ts'))
   .map((file) => file.slice(0, -3));
 
-export default inputs.map((input) => ({
+const getConfig = (input, minify) => ({
   input: `src/${input}.ts`,
   output: [{
-    file: `${input}.js`,
+    file: `${input}${minify ? '.min' : ''}.js`,
     format: 'es',
     sourcemap: !prod,
   }, {
-    file: `${input}.iife.js`,
+    file: `${input}.iife${minify ? '.min' : ''}.js`,
     format: 'iife',
     name: `dabolus.${input.replace(/-([a-z])/g, ([, l]) => l.toUpperCase())}`,
     globals: {
@@ -33,7 +35,30 @@ export default inputs.map((input) => ({
         },
       },
     }),
+    ...minify ? [
+      minifyHtml({
+        options: {
+          shouldMinify: (template) => /\s*<[a-z](?:[^`\\]|\\.)*>\s*/gm.test(template.parts[0].text),
+          minifyOptions: {
+            minifyCSS: true,
+            minifyJS: true,
+            collapseWhitespace: true,
+            collapseBooleanAttributes: true,
+            collapseInlineTagWhitespace: true,
+            removeOptionalTags: true,
+            removeTagWhitespace: true,
+            sortAttributes: true,
+            sortClassName: true,
+            removeRedundantAttributes: true,
+          }
+        }
+      }),
+      terser(),
+    ] : [],
   ],
   // Make all dependencies external
   external: () => true,
-}));
+});
+
+export default inputs.reduce((configs, input) =>
+  [...configs, getConfig(input, false), ...prod ? [getConfig(input, true)] : []], []);
