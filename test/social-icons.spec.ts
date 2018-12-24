9af6e3ca78fa5d6796589084312bc39a7579d904
Giorgio Garasto <giorgio@garasto.it>
2018-12-24 08:51:41 +0100

    test: Add a view box spec

--- test/social-icons.spec.ts
+++ test/social-icons.spec.ts
@@ -5,8 +5,12 @@ import * as socialIcons from '../src/social-icons';
 describe('Social Icons', () => {
   Object.entries(socialIcons).map(([key, value]: [string, string]) =>
     describe(`${key} icon`, () => {
-      it('should weigh less than 5kb', () => {
+      it('weighs less than 5kb', () => {
         expect(value.length).toBeLessThanOrEqual(5120);
+      });
+
+      it('has a view box of 256x256', () => {
+        expect(value).toContain('viewBox="0 0 256 256"');
       });
     }));
 });
