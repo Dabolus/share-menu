97adcab8479da6584d20bf52ef2c2c02934c78ee
Giorgio Garasto <giorgio@garasto.it>
2018-12-26 23:16:29 +0100

    test: Decrease max icon size to 3kb

--- test/social-icons.spec.ts
+++ test/social-icons.spec.ts
@@ -5,8 +5,8 @@ import * as socialIcons from '../src/social-icons';
 describe('Social Icons', () => {
   Object.entries(socialIcons).map(([key, value]: [string, string]) =>
     describe(`${key} icon`, () => {
-      it('weighs less than 5kb', () => {
-        expect(value.length).toBeLessThanOrEqual(5120);
+      it('weighs less than 3kb', () => {
+        expect(value.length).toBeLessThanOrEqual(3072);
       });
 
       it('has a view box of 256x256', () => {
