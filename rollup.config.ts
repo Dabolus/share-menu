ac2c773df1191ec0197d3fd60a7aa5921a9d862d
Giorgio Garasto <giorgio@garasto.it>
2019-01-12 19:06:36 +0100

    chore: Switch back to multioutput configuration

--- rollup.config.ts
+++ rollup.config.ts
@@ -11,65 +11,54 @@ const inputs = readdirSync('src')
   .filter((file) => !file.endsWith('.d.ts'))
   .map((file) => file.slice(0, -3));
 
-// TODO: reimplement old multioutput behavior as soon as terser plugins gets fixed
-// See: https://github.com/TrySound/rollup-plugin-terser/issues/5
-const getConfig = (input, minify) => {
-  const baseConfig = {
-    input: `src/${input}.ts`,
-    plugins: [
-      typescript({
-        tsconfigOverride: {
-          compilerOptions: {
-            declaration: prod,
-            sourceMap: !prod,
-          },
-        },
-      }),
-      ...minify ? [
-        minifyHtml({
-          options: {
-            shouldMinify: (template) => /\s*<[a-z](?:[^`\\]|\\.)*>\s*/gm.test(template.parts[0].text),
-            minifyOptions: {
-              minifyCSS: true,
-              minifyJS: true,
-              collapseWhitespace: true,
-              collapseBooleanAttributes: true,
-              collapseInlineTagWhitespace: true,
-              removeOptionalTags: true,
-              removeTagWhitespace: true,
-              sortAttributes: true,
-              sortClassName: true,
-              removeRedundantAttributes: true,
-            }
-          }
-        }),
-        terser(),
-      ] : [],
-    ],
-    // Make all dependencies external
-    external: () => true,
-  };
-
-  return [{
-    ...baseConfig,
-    output: {
-      file: `${input}${minify ? '.min' : ''}.js`,
-      format: 'es',
-      sourcemap: !prod,
-    },
+const getConfig = (input, minify) => ({
+  input: `src/${input}.ts`,
+  output: [{
+    file: `${input}${minify ? '.min' : ''}.js`,
+    format: 'es',
+    sourcemap: !prod,
   }, {
-    ...baseConfig,
-    output: {
-      file: `${input}.iife${minify ? '.min' : ''}.js`,
-      format: 'iife',
-      name: `dabolus.${input.replace(/-([a-z])/g, ([, l]) => l.toUpperCase())}`,
-      globals: {
-        [resolve(__dirname, `src/social-icons.js`)]: 'dabolus.socialIcons',
-      },
-      sourcemap: !prod,
+    file: `${input}.iife${minify ? '.min' : ''}.js`,
+    format: 'iife',
+    name: `dabolus.${input.replace(/-([a-z])/g, ([, l]) => l.toUpperCase())}`,
+    globals: {
+      [resolve(__dirname, `src/social-icons.js`)]: 'dabolus.socialIcons',
     },
-  }];
-};
+    sourcemap: !prod,
+  }],
+  plugins: [
+    typescript({
+      tsconfigOverride: {
+        compilerOptions: {
+          declaration: prod,
+          sourceMap: !prod,
+        },
+      },
+    }),
+    ...minify ? [
+      minifyHtml({
+        options: {
+          shouldMinify: (template) => /\s*<[a-z](?:[^`\\]|\\.)*>\s*/gm.test(template.parts[0].text),
+          minifyOptions: {
+            minifyCSS: true,
+            minifyJS: true,
+            collapseWhitespace: true,
+            collapseBooleanAttributes: true,
+            collapseInlineTagWhitespace: true,
+            removeOptionalTags: true,
+            removeTagWhitespace: true,
+            sortAttributes: true,
+            sortClassName: true,
+            removeRedundantAttributes: true,
+          }
+        }
+      }),
+      terser(),
+    ] : [],
+  ],
+  // Make all dependencies external
+  external: () => true,
+});
 
 export default inputs.reduce((configs, input) =>
-  [...configs, ...getConfig(input, false), ...prod ? [...getConfig(input, true)] : []], []);
+  [...configs, getConfig(input, false), ...prod ? [getConfig(input, true)] : []], []);
