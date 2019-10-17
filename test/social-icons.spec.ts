f284c03b175936944705fd5771526a646d935f74
Giorgio Garasto <giorgio@garasto.it>
2019-10-17 08:10:33 +0200

    test: Improve social icons specs

--- test/social-icons.spec.ts
+++ test/social-icons.spec.ts
@@ -4,16 +4,17 @@ import * as socialIcons from '../src/social-icons';
 describe('Social Icons', () => {
   Object.entries(socialIcons).map(([key, value]: [string, string]) =>
     describe(`${key} icon`, () => {
+      const openingTag = value.substring(0, value.indexOf('>'));
+
       it('weighs less than 1kb', () => {
-        expect(value.length).toBeLessThanOrEqual(1024);
+        expect(value.length).toBeLessThan(1024);
       });
 
       it('has a view box of 256x256', () => {
-        expect(value).toContain('viewBox="0 0 256 256"');
+        expect(openingTag).toContain('viewBox="0 0 256 256"');
       });
 
       it('does not have fixed width and height', () => {
-        const openingTag = value.substring(0, value.indexOf('>'));
         expect(openingTag).not.toContain('width');
         expect(openingTag).not.toContain('height');
       });
