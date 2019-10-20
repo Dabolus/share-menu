fca189b189867e6939dbcfe636fa522db607770c
Giorgio Garasto <giorgio@garasto.it>
2019-10-20 19:43:01 +0200

    test: Migrate social icons specs to open-wc testing

--- test/social-icons.spec.ts
+++ test/social-icons.spec.ts
@@ -1,22 +1,22 @@
-import '@skatejs/ssr/register';
+import { expect } from '@open-wc/testing';
 import * as socialIcons from '../src/social-icons';
 
-describe('Social Icons', () => {
+describe('social icons', () => {
   Object.entries(socialIcons).map(([key, value]: [string, string]) =>
     describe(`${key} icon`, () => {
       const openingTag = value.substring(0, value.indexOf('>'));
 
       it('weighs less than 1kb', () => {
-        expect(value.length).toBeLessThan(1024);
+        expect(value.length).to.be.below(1024);
       });
 
       it('has a view box of 256x256', () => {
-        expect(openingTag).toContain('viewBox="0 0 256 256"');
+        expect(openingTag).to.contain('viewBox="0 0 256 256"');
       });
 
       it('does not have fixed width and height', () => {
-        expect(openingTag).not.toContain('width');
-        expect(openingTag).not.toContain('height');
+        expect(openingTag).not.to.contain('width');
+        expect(openingTag).not.to.contain('height');
       });
     }),
   );
