fa25b12e6b992fe5e2bae2bdabd45b9ea3f7b2d9
Giorgio Garasto <giorgio@garasto.it>
2019-10-21 21:36:19 +0200

    test: Migrate share menu specs to open-wc testing

--- test/share-menu.spec.ts
+++ test/share-menu.spec.ts
@@ -1,7 +1,9 @@
-import '@skatejs/ssr/register';
+import { html, fixture, expect } from '@open-wc/testing';
+import { fake } from 'sinon';
+import '../src/share-menu';
 import { ShareMenu } from '../src/share-menu';
 
-// We need to do this because navigator.share and navigator.clipboard do not currently exist in TypeScript typings
+// We need to do this because navigator.share does not currently exist in TypeScript typings
 interface ShareOptions {
   url?: string;
   text?: string;
@@ -12,32 +14,34 @@ interface NavigatorWithShare extends Navigator {
   share: (options: ShareOptions) => Promise<void>;
 }
 
-declare const navigator: NavigatorWithShare;
+describe('share menu', async () => {
+  const shareMenu: ShareMenu = await fixture(html`
+    <share-menu></share-menu>
+  `);
 
-describe('Share Menu', () => {
-  const shareMenu = new ShareMenu();
-
-  describe('Native share via Web Share API', () => {
-    // undom does not provide navigator.share by default, so we have to mock it in this way
-    beforeAll(() => (navigator.share = () => Promise.resolve()));
-    afterAll(() => delete navigator.share);
+  describe('native share via Web Share API', async () => {
+    const fakeShare = fake(async () => {});
+    (window.navigator as NavigatorWithShare).share = fakeShare;
 
     it('uses the Web Share API if available', async () => {
-      await expect(shareMenu.share()).resolves.not.toThrow();
+      await expect(() => shareMenu.share()).not.to.throw();
+      expect(fakeShare.calledOnce).to.equal(true);
     });
 
-    it(`emits a 'share' event with 'native' as origin`, async () => {
-      const listener = jest.fn();
+    it("emits a 'share' event with 'native' as origin", async () => {
+      const listener = fake(({ detail: { origin } }: CustomEvent) => {
+        expect(origin).to.equal('native');
+      });
       shareMenu.addEventListener('share', listener);
       await shareMenu.share();
       shareMenu.removeEventListener('share', listener);
-      expect(listener).toBeCalledTimes(1);
+      expect(listener.calledOnce).to.equal(true);
     });
   });
 
-  describe('Share via fallback dialog', () => {
+  describe('share via fallback dialog', () => {
     it('defaults to all supported socials unless specified', () => {
-      expect(shareMenu.socials).toEqual(
+      expect(shareMenu.socials).to.deep.equal(
         Object.keys(shareMenu['_supportedSocials']),
       );
     });
