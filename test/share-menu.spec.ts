c3149176d834ff02ce3245f9c732fcddc08ea096
Giorgio Garasto <giorgio@garasto.it>
2019-10-23 08:19:39 +0200

    test: Better structure + add some specs

--- test/share-menu.spec.ts
+++ test/share-menu.spec.ts
@@ -14,14 +14,14 @@ interface NavigatorWithShare extends Navigator {
   share: (options: ShareOptions) => Promise<void>;
 }
 
-describe('share menu', async () => {
-  const shareMenu: ShareMenu = await fixture(html`
-    <share-menu></share-menu>
-  `);
-
+describe('share menu', () => {
   describe('native share via Web Share API', async () => {
     const fakeShare = fake(async () => {});
     (window.navigator as NavigatorWithShare).share = fakeShare;
+
+    const shareMenu: ShareMenu = await fixture(html`
+      <share-menu></share-menu>
+    `);
 
     it('uses the Web Share API if available', async () => {
       await expect(() => shareMenu.share()).not.to.throw();
@@ -40,10 +40,47 @@ describe('share menu', async () => {
   });
 
   describe('share via fallback dialog', () => {
-    it('defaults to all supported socials unless specified', () => {
-      expect(shareMenu.socials).to.deep.equal(
-        Object.keys(shareMenu['_supportedSocials']),
-      );
+    describe('socials', async () => {
+      const shareMenu: ShareMenu = await fixture(html`
+        <share-menu is-image="yes"></share-menu>
+      `);
+
+      it('defaults to all of them unless specified', () => {
+        expect(shareMenu.socials).to.deep.equal(
+          Object.keys(shareMenu['_supportedSocials']),
+        );
+      });
+
+      it('creates a button for each social', () => {
+        const buttons = shareMenu.shadowRoot.querySelectorAll<
+          HTMLButtonElement
+        >('button.social');
+
+        expect(buttons.length).to.equal(shareMenu.socials.length);
+        buttons.forEach((button, index) => {
+          expect(button.title).to.equal(
+            shareMenu['_supportedSocials'][shareMenu.socials[index]].title,
+          );
+        });
+      });
+
+      it('creates a button only for the specified socials and in the specified order', () => {
+        shareMenu.socials = ['facebook', 'skype', 'telegram'];
+        const buttons = shareMenu.shadowRoot.querySelectorAll<
+          HTMLButtonElement
+        >('button.social');
+
+        expect(buttons.length).to.equal(3);
+        expect(buttons[0].title).to.equal(
+          shareMenu['_supportedSocials'].facebook.title,
+        );
+        expect(buttons[1].title).to.equal(
+          shareMenu['_supportedSocials'].skype.title,
+        );
+        expect(buttons[2].title).to.equal(
+          shareMenu['_supportedSocials'].telegram.title,
+        );
+      });
     });
   });
 });
