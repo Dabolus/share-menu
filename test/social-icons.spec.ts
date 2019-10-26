f759541faa63012787b2db18248cb98b3430f450
Giorgio Garasto <giorgio@garasto.it>
2019-10-26 17:03:00 +0200

    test: Remove no more valid specs

--- test/social-icons.spec.ts
+++ test/social-icons.spec.ts
@@ -4,19 +4,8 @@ import * as socialIcons from '../src/social-icons';
 describe('social icons', () => {
   Object.entries(socialIcons).map(([key, value]: [string, string]) =>
     describe(`${key} icon`, () => {
-      const openingTag = value.substring(0, value.indexOf('>'));
-
       it('weighs less than 1kb', () => {
         expect(value.length).to.be.below(1024);
-      });
-
-      it('has a view box of 256x256', () => {
-        expect(openingTag).to.contain('viewBox="0 0 256 256"');
-      });
-
-      it('does not have fixed width and height', () => {
-        expect(openingTag).not.to.contain('width');
-        expect(openingTag).not.to.contain('height');
       });
     }),
   );
