238aa5b2d2f82800cb1d25ed6316f402375754f9
Giorgio Garasto <giorgio@garasto.it>
2020-05-06 21:46:01 +0200

    feat: use dialog share api if possible and remove deprecated parameter

--- src/targets/facebook.ts
+++ src/targets/facebook.ts
@@ -6,19 +6,41 @@ export class FacebookShareTarget extends HTMLElement implements ShareTarget {
   public readonly icon =
     'M94 50v35H68v43h26v128h53V128h36l5-43h-41V55c0-4 6-10 12-10h29V0h-40C93 0 94 43 94 50z';
 
+  /**
+   * The Facebook App ID. If the Facebook JS SDK is not found and this parameter
+   * is specified, then the Facebook share dialog will be used instead of the sharer
+   * API to make it possible to also share the text and title together with the URL.
+   *
+   * @return {string}
+   */
+  public get appId(): string {
+    return this.getAttribute('app-id');
+  }
+
+  public set appId(val: string) {
+    this.setAttribute('app-id', val);
+  }
+
   public share(shareMenu: ShareMenu) {
     if (window.FB) {
       window.FB.ui({
-        href: shareMenu.url,
         method: 'share',
-        mobile_iframe: true, // eslint-disable-line @typescript-eslint/camelcase
-        quote: shareMenu.text,
+        href: shareMenu.url,
+        quote: `${shareMenu.title}\n${shareMenu.text}`,
       });
     } else {
       shareMenu.openWindow(
-        `https://www.facebook.com/sharer.php?u=${encodeURIComponent(
-          shareMenu.url,
-        )}`,
+        this.appId
+          ? `https://www.facebook.com/dialog/share?app_id=${
+              this.appId
+            }&display=popup&href=${encodeURIComponent(
+              shareMenu.url,
+            )}&quote=${encodeURIComponent(
+              shareMenu.title,
+            )}%0A${encodeURIComponent(shareMenu.text)}`
+          : `https://www.facebook.com/sharer.php?u=${encodeURIComponent(
+              shareMenu.url,
+            )}`,
       );
     }
   }
