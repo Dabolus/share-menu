d6340d8e904d40057cb862acee28d705f0139263
Giorgio Garasto <giorgio@garasto.me>
2022-12-11 13:45:44 +0100

    test: run tests sequentially & w/ playwright

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,7 +1,10 @@
 import { esbuildPlugin } from '@web/dev-server-esbuild';
+import { playwrightLauncher } from '@web/test-runner-playwright';
 
 export default {
+  concurrency: 1,
   nodeResolve: true,
   files: ['src/**/*.test.ts', 'src/**/*.spec.ts'],
   plugins: [esbuildPlugin({ ts: true })],
+  browsers: [playwrightLauncher({ product: 'chromium' })],
 };
