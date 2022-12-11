9cac20bf52d1d96a4be9aad6edd037970dcc3c38
Giorgio Garasto <giorgio@garasto.me>
2022-12-11 14:26:28 +0100

    chore: drop useless if condition

--- src/targets/print.ts
+++ src/targets/print.ts
@@ -7,7 +7,7 @@ export class PrintShareTarget extends HTMLElement implements ShareTarget {
     'M217 77H39a38 38 0 0 0-38 38v77h51v50h152v-50h51v-77a38 38 0 0 0-38-38zm-38 140H77v-64h102zm38-89a13 13 0 1 1 13-13 13 13 0 0 1-13 13zM204 14H52v51h152z';
 
   public share(shareMenu: ShareMenu) {
-    if (!shareMenu.url || shareMenu.url === window.location.href) {
+    if (shareMenu.url === window.location.href) {
       shareMenu.addEventListener('close', () => window.print(), {
         once: true,
       });
