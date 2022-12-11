3a12ee5dccbedcb9a717c604a2f1bb2f3ad66350
Giorgio Garasto <giorgio@garasto.me>
2022-12-11 20:39:35 +0100

    chore: add space in hacker news name

--- src/targets/hacker-news.ts
+++ src/targets/hacker-news.ts
@@ -1,7 +1,7 @@
 import type { ShareMenu, ShareTarget } from '../share-menu';
 
 export class HackerNewsShareTarget extends HTMLElement implements ShareTarget {
-  public readonly displayName = 'HackerNews';
+  public readonly displayName = 'Hacker News';
   public readonly color = 'fb651e';
   public readonly icon =
     'M112 157L31 5h37l48 96 2 5a50 50 0 013 6 11 11 0 011 2l1 2 3 7 3 6 6-13 7-15 48-96h35l-82 153v98h-31z';
