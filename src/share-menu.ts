b89dfb28b4c047a91fcc2afd3504e60c5c3e3a60
Giorgio Garasto <giorgio@garasto.me>
2023-08-14 08:39:51 +0200

    fix: wrong clipboard preview markup generation

--- src/share-menu.ts
+++ src/share-menu.ts
@@ -764,7 +764,7 @@ export class ShareMenu extends HTMLElement {
       case 'title':
       case 'url':
         if (this._clipboardPreviewRef) {
-          this._clipboardPreviewRef.innerHTML = `${this.title}<br>${this.text}<br>${this.url}`;
+          this._clipboardPreviewRef.innerHTML = `${this.title}\n${this.text}\n${this.url}`;
         }
         break;
       case 'image':
